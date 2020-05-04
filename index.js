784b231b00845c653d70857c714390e6382f6483
Bashnona Gamal <b.d.fire@hotmail.com>
2020-05-04 21:06:38 +0200

    Chat.js is created

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ app.use(express.static('public'));
 var io = socket(server);
 
 io.on('connection', function(socket){
-    console.log('Connection Created Successfuly!');
+    console.log('Connection Created Successfuly!', socket.id);
 })
